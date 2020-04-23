03aeb4b28cb93c1090e216c7249251bd9f511301
Yogita Mahle <mahaleyogita233@gmail.com>
2020-04-23 15:13:18 +0530

    Index - Employee controller

---
+++ plethocoreProject/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using plethocoreProject.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace plethocoreProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeServices _employeeservices;
+        
+
+        public EmployeeController(IEmployeeServices employeeservices)
+        {
+            _employeeservices = employeeservices;
+
+        }
+        public IActionResult Index()
+        {
+            var emp = _employeeservices.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                ImageUrl = employee.ImageUrl,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                Designation = employee.Designation,
+                city = employee.City,
+                DateofJoined = employee.DateofJoined
+
+            }).ToList();
+            return View(emp);
+        }
+    }
+}
---
+++ plethocoreProject/Models/EmployeeIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace plethocoreProject.Models
+{
+    public class EmployeeIndexViewModel
+    {
+        public int Id { get; set; }
+        public string EmployeeNo { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DateofJoined { get; set; }
+        public string Designation { get; set; }
+        public string city { get; set; }
+
+    }
+}
